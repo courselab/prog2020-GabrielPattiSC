9a1c74de511a9a20eb285706eb04e8b4355d0180
GabrielPattiSC <gabriel.patti@usp.br>
2020-06-23 22:23:24 +0000

    m003 concluido

--- m003.c
+++ m003.c
@@ -23,6 +23,18 @@
 
 int is_prime (int a)
 {
+    int i, count = 0;
+
+    for (i = 2; i <= a / 2; i++) {
+        if (a % i == 0) {
+            count++;
+        
+        }
+    }
+
+    if (count == 0)
+        return 1;
+    else
   return 0;
 }
 
